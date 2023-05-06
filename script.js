23fbf0779f51dc397e527d6727806caa56f9e524
DimaSusha <mityasusha@gmail.com>
2023-05-06 19:37:10 +0300

    section cosmetics and contacts update

--- script.js
+++ script.js
@@ -160,6 +160,63 @@ btnAccordions.forEach((btnAccordion, index) => {
 });
 
 
+/***********COSMETICS SECTION*********/ 
+
+const carousel = document.querySelector(".carousel");
+const arrowIcons = document.querySelectorAll(".fa-solid");
+const firstImg = carousel.querySelectorAll(".composition__photo")[0];
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+
+const showHideIcons = () => {
+  // showing and hiding prev/next icon according to carousel scroll left value
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display = carousel.scrollLeft >= scrollWidth ? "none" : "block";
+};
+
+arrowIcons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 12;
+    // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60); //calling showHideIcons after 60ms
+  });
+});
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) =>  {
+  //scrolling images to left according to mouse pointer
+  if(!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+}
+
+
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
 
 /***********CONTACTS SECTION*********/ 
 
